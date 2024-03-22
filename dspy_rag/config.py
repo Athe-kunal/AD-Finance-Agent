b5a46ce67ef6b8028f691fdfb4a0cb8c84ee6795
Athe-kunal <athekunal@gmail.com>
2024-03-21 15:19:39 -0900

    dspy python files

---
+++ dspy_rag/config.py
@@ -0,0 +1,13 @@
+# EMBEDDING_MODEL = "text-embedding-3-large"
+# EMBEDDING_MODEL = "text-embedding-3-small"
+# EMBEDDING_MODEL = "nomic-ai/nomic-embed-text-v1"
+EMBEDDING_MODEL = "mixedbread-ai/mxbai-embed-large-v1"
+COLLECTION_NAME = "ad-project"
+# DB_PATH = "AD-DB-NOMIC-v1"
+BASE_DATABASE_NAME = "AD-DB"
+# EMBEDDING_SOURCE = "openai" 
+EMBEDDING_SOURCE = "hf" 
+TOP_K = 5
+HYDE_MODEL = "gpt-3.5-turbo"
+TOKENIZER_MODEL = "distilgpt2"
+MOD_HYDE_MODEL = "distilgpt2-ad-test"
---
+++ dspy_rag/database.py
@@ -0,0 +1,35 @@
+from dspy_rag.config import *
+from llama_index.core import VectorStoreIndex
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from llama_index.legacy.embeddings.openai import OpenAIEmbedding
+from llama_index.legacy.embeddings.huggingface import HuggingFaceEmbedding
+import chromadb
+
+def load_database(embedding_source:str,k):
+    if "mxbai" in EMBEDDING_MODEL:
+        database_name_path = BASE_DATABASE_NAME + "-MXBAI"
+        collection_name = COLLECTION_NAME 
+    elif "large" in EMBEDDING_MODEL and embedding_source=="openai":
+        database_name_path = BASE_DATABASE_NAME + "LARGE"
+        collection_name = COLLECTION_NAME + "-large"
+    elif "small" in EMBEDDING_MODEL:
+        database_name_path = BASE_DATABASE_NAME + "SMALL"
+        collection_name = COLLECTION_NAME + "-small"
+    elif "nomic" in EMBEDDING_MODEL:
+        database_name_path = BASE_DATABASE_NAME + "-NOMIC-v1"
+        collection_name = COLLECTION_NAME 
+    # print(collection_name)
+    db2 = chromadb.PersistentClient(path=database_name_path)
+    if embedding_source == "openai":
+        embed_model = OpenAIEmbedding(model=EMBEDDING_MODEL,api_key=os.environ['OPENAI_API_KEY'])
+    elif embedding_source == 'hf':
+        embed_model = HuggingFaceEmbedding(model_name=EMBEDDING_MODEL,trust_remote_code=True,device='cuda:0')
+
+    chroma_collection = db2.get_collection(collection_name)
+    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+    index_ = VectorStoreIndex.from_vector_store(
+        vector_store,
+        embed_model=embed_model,
+    )
+    retriever = index_.as_retriever(similarity_top_k=k*5)
+    return retriever
